884f51ed8215e0f58181e667ec3912b77bb1b18b
Antonova MArgarita <rita@rita321.ru>
2024-01-05 00:25:49 +0300

    добавлен запрос на кол-во персон

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner (System.in);
+
+        int persons;
+
+            System.out.println("На сколько человек будем делить счет?");
+            persons = scanner.nextInt();
+
+            while (persons <= 1) {
+                System.out.println("Введено некорректное значение, повторите попытку");
+                persons = scanner.nextInt();
+            }
+
+        }
     }
-}
