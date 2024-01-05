7e751c714e4629ff18747da94d52bed166ebe9ca
Antonova MArgarita <rita@rita321.ru>
2024-01-05 18:53:40 +0300

    реализовано округление финального числа и склонение рублей

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,63 @@
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int persons;
+        int persons = 0;
         String productName;
         double sumCh = 0;
         String exit = "завершить";
         String checklist = "";
+        double pieToPerson;
+        String rub = "";
 
 
-            System.out.println("На сколько человек будем делить счет?"); // определяем количество персон
+        System.out.println("На сколько человек будем делить счет?"); // определяем количество персон
+        persons = scanner.nextInt();
+        while (persons <= 1) {
+            System.out.println("Введено некорректное значение, повторите попытку");
             persons = scanner.nextInt();
-            while (persons <= 1) {
-                System.out.println("Введено некорректное значение, повторите попытку");
-                persons = scanner.nextInt();
+        }
+
+
+        while (true) {                                                  // запрос названия
+            Product product = new Product();
+            System.out.println("Введите название товара или 'завершить'");
+            product.name = scanner.next();
+
+            if (exit.equalsIgnoreCase(product.name)) {                  // выход из списка
+                break;
             }
+            checklist = checklist + product.name + "\n";
 
+            System.out.println("Введите стоимость товара");             // запрос цены
+            product.price = scanner.nextDouble();
 
-            while (true) {                                                  // запрос названия
-                Product product = new Product();
-                System.out.println("Введите название товара или 'завершить'");
-                product.name = scanner.next();
+            sumCh = sumCh + product.price;                              // общая сумма
 
-                if (exit.equalsIgnoreCase(product.name)) {                  // выход из списка
-                    break;
-                }
-                checklist = checklist + product.name+ ", ";
-
-                System.out.println("Введите стоимость товара");             // запрос цены
-                product.price = scanner.nextDouble();
-
-                sumCh = sumCh+product.price;                                // общая сумма
-                System.out.println("Продукт успешно добавлен в список");
-
-
-            }
-        System.out.println("Полный список покупок: "+checklist);
-        System.out.println("Общая сумма покупки равна "+sumCh+"руб., каждый должен внести по "+sumCh/persons+" руб.");
-
-
+            System.out.println("Продукт успешно добавлен в список");
 
 
         }
+
+        pieToPerson = sumCh / persons;
+
+        if (pieToPerson % 100 > 4 && pieToPerson % 100 < 21) {
+            rub = "рублей";
+        } else if (pieToPerson % 10 == 1) {
+            rub = "рубль";
+        } else if (pieToPerson % 10 > 1 && pieToPerson % 10 < 5) {
+            rub = "рубля";
+        } else rub = "рублей";
+
+
+        System.out.println("Добавленные товары:\n" + checklist);
+        System.out.println("Каждый должен внести по " + String.format("%.2f", pieToPerson) + " " + rub);
     }
+}
+
+
+
+
 
