bc7611d2d27c13c4f8783a62554dd98164b2b304
Antonova MArgarita <rita@rita321.ru>
2024-01-05 14:36:36 +0300

    добавлен запрос названия, цены, сложения общей суммы и выход из списка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,38 @@ public class Main {
         Scanner scanner = new Scanner (System.in);
 
         int persons;
+        String productName;
+        double sumCh = 0;
+        String exit = "завершить";
 
-            System.out.println("На сколько человек будем делить счет?");
+
+            System.out.println("На сколько человек будем делить счет?"); // определяем количество персон
             persons = scanner.nextInt();
-
             while (persons <= 1) {
                 System.out.println("Введено некорректное значение, повторите попытку");
                 persons = scanner.nextInt();
             }
 
+
+            while (true) {                                                  // запрос названия, цены, подсчет суммы
+                Product product = new Product();
+                System.out.println("Введите название товара или 'завершить'");
+                product.name = scanner.next();
+                if (exit.equalsIgnoreCase(product.name)) {
+                    break;
+                }
+                System.out.println("Введите стоимость товара");
+                product.price = scanner.nextDouble();
+                sumCh = sumCh+product.price;
+                System.out.println("Продукт успешно добавлен в список");
+
+
+            }
+        System.out.println("Подводим итог");
+        System.out.println(sumCh/persons);
+
+
+
         }
     }
+
---
+++ src/main/java/Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    String name;
+    double price;
+
+    public Product() {
+        String nam;
+        double pri;
+    }
+}
