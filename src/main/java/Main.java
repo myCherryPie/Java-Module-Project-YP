e47eaf2994454b8fa3904d8e2c3f86a7029775bf
Antonova MArgarita <rita@rita321.ru>
2024-01-07 12:06:29 +0300

    сделано все, допилить проверку на отрицательность

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,69 +1,116 @@
+import static java.text.ChoiceFormat.nextDouble;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int persons = 0;
         String name = "";
         String checklist = "";
         String exit = "завершить";
         double price = 0;
         double sumCh = 0;
-        double pieToPerson = sumCh / persons;
+
         String rub = "";
-        String num = "";
+        int persons = 0;
+
+//begin---------------------------------------------------------------------------------------------
+        System.out.println("На сколько человек будем делить счет?");
+       // testPersons(persons,scanner);
+
+        boolean testPersons = scanner.hasNextInt();
+        if (testPersons) {
+            persons = scanner.nextInt();
+
+        } else {
+            System.out.println("введите коректное значение больше 1");
+            testPersons(persons,scanner);
+        }
+
+        while (true) {
+            System.out.println("Введите название товара или 'завершить'");
+            Scanner scanner1 = new Scanner(System.in);
+            name = scanner1.nextLine();
+            if (exit.equalsIgnoreCase(name)) {
+                break;
+            }
+            else checklist = checklist + name + "\n";
+
+            System.out.println("Введите стоимость товара");
+            Scanner scanner2 = new Scanner(System.in);
+            boolean testDouble = scanner2.hasNextDouble();
+            if (testDouble) {
+                price = scanner2.nextDouble();
+                if (price > 0) {
+                    sumCh = sumCh + price;
+                    System.out.println("продукт добавлен");
+                } else
+
+            } else {
+                System.out.println("введите корректное и положительное значение");
+                testPrice(price,scanner,sumCh);
+            }
+         }
+
+        System.out.println("Общая сумма покупки: "+sumCh);
+
+        double pieToPerson = sumCh / persons;
+
+       getRub(pieToPerson,rub, checklist);
+    }
+// end script-------------------------------------------------------------------------------
+
+    public static void getPersons(int persons,Scanner scanner){
+        Scanner scanner8 = new Scanner(System.in);
 
         System.out.println("На сколько человек будем делить счет?");
-        persons = scanner.nextInt();
+        persons = scanner8.nextInt();
         while (persons <= 1) {
             System.out.println("Введено некорректное значение, повторите попытку");
             persons = scanner.nextInt();
         }
-
-        while (true) {
-            getName(name, checklist, exit);
-            if (exit.equalsIgnoreCase(name)) {                  // выход из списка
-                break;
-            }
-            checklist = checklist + name + "\n";
-            getPrice(scanner,price,sumCh,num);
-        }
-        getRub(pieToPerson,rub, checklist);
-
-
-
     }
 
-
-    public static void getName(String name, String checklist, String exit) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите название товара или 'завершить'");
-        name = scanner.nextLine();
-
-    }
-
-    public static void getPrice(Scanner scanner, double price, double sumCh, String num) {                  //цена
-        System.out.println("Введите стоимость товара");
-        testDouble(price);
-        sumCh = sumCh + price;
-        System.out.println("Продукт успешно добавлен в список");
-        }
-    static public void testDouble(double price) {
-        Scanner scan = new Scanner(System.in);
-        boolean testDouble = scan.hasNextDouble();
-        if (testDouble) {
-            price = scan.nextDouble();
+    static public void testPrice(double price, Scanner scanner, double sumCh) {
+        Scanner scanner2 = new Scanner(System.in);
+        boolean testDouble = scanner2.hasNextDouble();
+        if (testDouble && price >= 0) {
+            price = scanner2.nextDouble();
+            sumCh=sumCh+price;
+            System.out.println("продукт добавлен");
         } else {
-            System.out.println("введите коректное значение");
-            testDouble(price);
+            System.out.println("введите корректное и положительное значение");
+            testPrice(price,scanner,sumCh);
         }
     }
 
+    static public void testPersons(int persons, Scanner scanner) {
+     //   Scanner scanner = new Scanner(System.in);
+        boolean testPersons = scanner.hasNextInt();
+        if (testPersons && persons > 1) {
+            persons = scanner.nextInt();
+return;
+        } else {
+            System.out.println("введите коректное значение больше 1");
+            testPersons(persons,scanner);
+        }
+    }
+
+    static public void testMinus(double price, Scanner scanner, double sumCh) {
+       int prc = (int)Math.ceil(price);
+        if (prc<0) {
+            System.out.println("введите положительное значение");
+            price=scanner.nextDouble();
+        } else {
+            System.out.println("Введите положительное число");
+            sumCh = sumCh + price;
+            System.out.println("Продукт успешно добавлен в список");
+        }
+    }
 
     public static void getRub(double pieToPerson, String rub, String checklist) {
         System.out.println("Добавленные товары:\n" + checklist);
-        System.out.println("Каждый должен внести по " + String.format("%.2f", pieToPerson) + " " + rub);
         if (pieToPerson % 100 > 4 && pieToPerson % 100 < 21) {
             rub = "рублей";
         } else if (pieToPerson % 10 == 1) {
@@ -71,7 +118,10 @@ public class Main {
         } else if (pieToPerson % 10 > 1 && pieToPerson % 10 < 5) {
             rub = "рубля";
         } else rub = "рублей";
+        System.out.println("Каждый должен внести по " + pieToPerson + " " + rub);// System.out.println("Каждый должен внести по " + String.format("%.2f", pieToPerson) + " " + rub);
+
     }
+    //end class Main
 }
 
 
